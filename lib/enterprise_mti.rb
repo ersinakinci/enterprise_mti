e7177270210e549680f43040b21b8c4497d43cc4
Ersin Akinci <ersin@Collin-Bonds-MacBook-Pro.local>
2013-10-29 22:12:35 -0500

    Refactored code from other project, began adding specs

--- lib/enterprise_mti.rb
+++ lib/enterprise_mti.rb
@@ -1,2 +1,9 @@
+require 'enterprise_mti/class_methods'
+require 'enterprise_mti/migration'
+
 module EnterpriseMti
+  extend ActiveSupport::Concern
 end
+
+ActiveRecord::Base.send :include, ::EnterpriseMti
+ActiveRecord::Migration.send :include, ::EnterpriseMti::Migration
